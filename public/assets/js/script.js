d336d1f7d34804e2773f514eb9549d6ae68d4d70
Anthony <anthonyguitarman13@gmail.com>
2016-12-10 14:20:16 -0700

    more stuff

---
+++ public/assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$("#submit").click(function(){
+  event.preventDefault();
+  var summonerName = $(".summoner").val();
+  console.log(summonerName)
+
+})
+
+
+})
+console.log("Hi")
