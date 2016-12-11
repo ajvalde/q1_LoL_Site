a51ad978e1d0625794a821138dd4549b91007b4e
Anthony <anthonyguitarman13@gmail.com>
2016-12-10 17:25:57 -0700

    got player id - still needs work

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,10 +3,21 @@ $(document).ready(function(){
 $("#submit").click(function(){
   event.preventDefault();
   var summonerName = $(".summoner").val();
-  console.log(summonerName)
 
+  var apiKey = ""
+  // var sumId = $.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey)
+  // console.log(sumId)
+  getId(summonerName,apiKey)
 })
 
 
 })
-console.log("Hi")
+
+ function getId (sumName,key){
+     var sumId = $.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + sumName + "?api_key=" + key, function(data){
+     var name = $(".summoner").val();
+     var playerId = $("#" + data.synstar.id)
+       console.log(playerId)
+       console.log("name " + name)
+   })
+ }
