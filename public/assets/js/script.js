df83e032ce824833b338f82d2ceb64eda61f9d3b
Anthony <anthonyguitarman13@gmail.com>
2016-12-12 13:06:42 -0700

    getId function revised

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -15,22 +15,20 @@ $("#submit").click(function(){
  function getId (sumName,key){
      var sumId = $.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + sumName + "?api_key=" + key, function(data){
      var name = $(".summoner").val();
-     var playerId = $(data[name].id)
+     var playerIdObj = $(data[name].id)
+     var playerId = playerIdObj["0"]
      var api = key
-     getStats(key)
-     //console.log(playerId)
-     //console.log(data.synstar.id)
+     getStats(playerId,key)
+
+
    })
  }
 
- function getStats(key){
-   var sumStats = $.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/19341022/summary?season=SEASON2016&api_key=" +key, function(data){
+ function getStats(playerId,key){
+   var sumStats = $.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + playerId + "/summary?season=SEASON2016&api_key=" +key, function(data){
      for(var i=0; i < data.playerStatSummaries.length; i++){
 
        console.log(data.playerStatSummaries[i])
      }
    })
-
-
-
  }
