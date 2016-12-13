7783592d0c0ab45ba2a1f79d66bce757da34b4b4
Anthony <anthonyguitarman13@gmail.com>
2016-12-13 16:04:06 -0700

    JavaScript working

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,14 +28,14 @@ $(".summoner").keyup(function(e){
      var api = key
      getUnrankedStats(playerId,key)
      getRankedStats(playerId,key)
-
+     getAramStats(playerId,key)
    })
  }
 
  function getUnrankedStats(playerId,key){
    var sumStats = $.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + playerId + "/summary?season=SEASON2016&api_key=" +key, function(data){
      for(var i=0; i < data.playerStatSummaries.length; i++){
-       //console.log(data.playerStatSummaries[i])
+       console.log(data.playerStatSummaries[i])
        if (data.playerStatSummaries[i]["playerStatSummaryType"] === "Unranked"){
          $(".uwin_val").text(data.playerStatSummaries[i].wins)
          $(".uchamp_val").text(data.playerStatSummaries[i].aggregatedStats.totalChampionKills)
@@ -57,12 +57,24 @@ $(".summoner").keyup(function(e){
          $(".rchamp_val").text(data.playerStatSummaries[i].aggregatedStats.totalChampionKills)
          $(".rassist_val").text(data.playerStatSummaries[i].aggregatedStats.totalAssists)
          $(".rminion_val").text(data.playerStatSummaries[i].aggregatedStats.totalMinionKills)
-       } else{
+       }
+       else if(data.playerStatSummaries[i]["wins"] === "0"){
          $(".rwin_val").text("No STATS")
          $(".rchamp_val").text("No STATS")
          $(".rassist_val").text("No STATS")
          $(".rminion_val").text("No STATS")
+       }
+     }
+   })
+ }
 
+ function getAramStats(playerId,key){
+   var ranked =$.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + playerId + "/summary?season=SEASON2016&api_key=" +key, function(data){
+     for(var i=0; i < data.playerStatSummaries.length; i++){
+       if(data.playerStatSummaries[i]["playerStatSummaryType"] === "AramUnranked5x5"){
+         $(".awin_val").text(data.playerStatSummaries[i].wins)
+         $(".achamp_val").text(data.playerStatSummaries[i].aggregatedStats.totalChampionKills)
+         $(".aassist_val").text(data.playerStatSummaries[i].aggregatedStats.totalAssists)
        }
      }
    })
